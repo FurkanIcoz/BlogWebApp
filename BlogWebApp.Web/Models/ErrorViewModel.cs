a2589c64f69923a1a1063b6231ebf2cddb05b32a
furkanicoz <furkanicoz@outlook.com>
2023-11-10 01:59:32 +0300

    Add project files.

---
+++ BlogWebApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogWebApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
